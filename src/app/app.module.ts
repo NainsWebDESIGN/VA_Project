77eeecf3ba354905db2c549ab173c627ab3bb43c
Nains <jason20226@gmail.com>
2021-03-28 21:43:45 +0800

    2021-03-28 21:44

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,14 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { LoadComponent } from './load/load.component';
 
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [	
+    AppComponent,
+      LoadComponent
+   ],
   imports: [
     BrowserModule
   ],
---
+++ src/app/load/load.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'load',
+  templateUrl: './load.component.html',
+  styleUrls: ['./load.component.css']
+})
+export class LoadComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
