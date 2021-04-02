97a2f76524534acc3a5577bad76fd1b2f8623c73
Nains <jason20226@gmail.com>
2021-04-02 19:06:45 +0800

    2021-04-02 19:06

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -12,24 +12,13 @@ export class ContactComponent implements OnInit {
   message: string;
   constructor(private api: ApiService) { }
   Test() {
-    const data = {
-      "entry.2002706790": this.name,
-      "entry.1995154974": this.email,
-      "entry.2137997242": this.message
-    }
-    let Observer = {
-      next: el => { console.log(el.ok); },
-      error: err => { console.log(err); },
-      complete: () => { console.log('OK'); }
-    }
+    const data = { "entry.2002706790": this.name, "entry.1995154974": this.email, "entry.2137997242": this.message }
+    let Observer = { next: el => { console.log(el.ok); }, error: err => { console.log(err); }, complete: () => { console.log('OK'); } }
     let myreg = /^[^\[\]\(\)\\<>:;,@.]+[^\[\]\(\)\\<>:;,@]*@[a-z0-9A-Z]+(([.]?[a-z0-9A-Z]+)*[-]*)*[.]([a-z0-9A-Z]+[-]*)+$/g; // 信箱驗證格式
-    if (this.name.trim() == '' || this.email.trim() == '' || this.message.trim() == '') {
-      alert('請務必填寫完整');
-    } else if (!myreg.test(this.email)) {
-      alert('信箱格式錯誤')
-    } else {
-      this.api.postApi(131, data).subscribe(Observer);
-    }
+
+    if (this.name.trim() == '' || this.email.trim() == '' || this.message.trim() == '') { alert('請務必填寫完整'); }
+    else if (!myreg.test(this.email)) { alert('信箱格式錯誤') }
+    else { this.api.postApi(131, data).subscribe(Observer); }
   }
   ngOnInit() {
   }
