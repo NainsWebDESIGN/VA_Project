0899b5f16ad6cfb0bcb531fe4b8524765b06c482
Nains <jason20226@gmail.com>
2021-04-11 22:55:10 +0800

    2021-04-11 22:55

--- README.md
+++ README.md
@@ -7,3 +7,4 @@
 1. 清理緩存 `npm cache clean --force`
 2. 重新 `npm i`
 3. 新增 servicePage 各項目頭頂條例
+4. 新增價格樣式（無資料）
--- src/app/servicePage/servicePage.component.ts
+++ src/app/servicePage/servicePage.component.ts
@@ -9,7 +9,25 @@ import { ApiService } from '@service/api.service';
 })
 export class ServicePageComponent implements OnInit {
   data: Array<any> = [];
+  filterWorks: Array<any> = ["totle", "one", "two", "three"];
+  finalWork: Array<string> = ["one", "two", "three"];
+  Choice: boolean = false;
   constructor(private infor: Information, private api: ApiService) { }
+  Price() {
+    this.Choice = !this.Choice;
+  }
+  filterWork(_Type: string) {
+    this.finalWork = [];
+    switch (_Type) {
+      case 'totle':
+        this.finalWork = ["one", "two", "three"];
+      default:
+        for (let i = 0; i < this.data[2].length; i++) {
+          let type = this.data[2][i].type
+          if (type == _Type) { this.finalWork.push(type) }
+        }
+    }
+  }
   test() {
     let animate = this.data[1].right;
     for (let i = 0; i < animate.length; i++) {
@@ -27,6 +45,7 @@ export class ServicePageComponent implements OnInit {
       let link = this.infor.pageLink[2];
       (link == 'SerTop') ? scroll(0, 0) : document.getElementById(link).scrollIntoView({ behavior: 'smooth' });
 
+      this.test();
     })
   }
 
