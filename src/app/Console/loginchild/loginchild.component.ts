add475c2ab8d7f66991c666e9a396790b55505ca
Nains <jason20226@gmail.com>
2021-04-16 18:02:40 +0800

    2021-04-16 18:02

--- src/app/Console/loginchild/loginchild.component.ts
+++ src/app/Console/loginchild/loginchild.component.ts
@@ -8,13 +8,31 @@ import { ApiService } from '@service/api.service';
 })
 export class LoginAbout implements OnInit {
   data: Array<any> = [];
+  formData: any = {
+    Team: { name: '', type: '', pic: '', content: '' },
+    Place: { name: '', style: '', content: '' }
+  };
   check: any = {
     Team: [],
     Place: []
-  }
+  };
   constructor(private api: ApiService) { }
   Check(_Position: number, _Item: string) {
     this.check[_Item][_Position] = !this.check[_Item][_Position];
+  }
+  Submit(_Need: string, _Item: string) {
+    console.log(this.formData[_Item]);
+    // switch(_Need){
+    //   case 'Add':
+    //   case 'Update':
+    // }
+  }
+  Delete(_Item: string, _Data: number) {
+    let data = [];
+    for (let i = 0; i < this.check[_Item].length; i++) {
+      if (!this.check[_Item][i]) { data.push(this.data[_Data][i]); }
+    }
+    console.log(data);
   }
   ngOnInit() {
     this.api.postApi('about').subscribe((el: Array<any>) => {
@@ -44,6 +62,27 @@ export class LoginMessage implements OnInit {
   }
   ngOnInit() {
     this.api.postApi('message').subscribe((el: Array<any>) => {
+      this.data = el;
+      this.data.forEach(el => { this.check.push(true); });
+    })
+  }
+
+}
+
+@Component({
+  selector: 'loginContact',
+  templateUrl: './loginContact.html',
+  styleUrls: ['./loginchild.component.css']
+})
+export class LoginContact implements OnInit {
+  data: any = [];
+  check: Array<boolean> = [];
+  constructor(private api: ApiService) { }
+  Check(_Position: number) {
+    this.check[_Position] = !this.check[_Position];
+  }
+  ngOnInit() {
+    this.api.postApi('contact').subscribe((el: Array<any>) => {
       this.data = el;
       this.data.forEach(el => { this.check.push(true); });
     })
@@ -87,27 +126,6 @@ export class LoginService implements OnInit {
       this.data[2].forEach(el => { this.check.Portofio.push(true); });
       this.data[3].month.forEach(el => { this.check.month.push(true); });
       this.data[3].year.forEach(el => { this.check.year.push(true); });
-    })
-  }
-
-}
-
-@Component({
-  selector: 'loginContact',
-  templateUrl: './loginContact.html',
-  styleUrls: ['./loginchild.component.css']
-})
-export class LoginContact implements OnInit {
-  data: any = [];
-  check: Array<boolean> = [];
-  constructor(private api: ApiService) { }
-  Check(_Position: number) {
-    this.check[_Position] = !this.check[_Position];
-  }
-  ngOnInit() {
-    this.api.postApi('contact').subscribe((el: Array<any>) => {
-      this.data = el;
-      this.data.forEach(el => { this.check.push(true); });
     })
   }
 
