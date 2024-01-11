78db9445ebc1404041e60c3c7c38c7878067e76a
Nains <jason20226@gmail.com>
2024-01-11 15:50:26 +0800

    2024-01-11 15:50

--- src/app/component.module.ts
+++ src/app/component.module.ts
@@ -1,17 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { MessageComponent } from './message/message.component';
+// import { MessageComponent } from './message/message.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
   declarations: [
-    MessageComponent
+    // MessageComponent
   ],
   exports: [
-    MessageComponent
+    // MessageComponent
   ]
 })
 export class ComponentModule { }
